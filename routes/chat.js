8ee3997301e8d7ff24c08697b3199b548f5fcdda
Alex Lamarche <alexandre_perso@outlook.com>
2023-02-05 09:57:00 -0500

    udpates

--- routes/chat.js
+++ routes/chat.js
@@ -2,10 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/:emoji", function (req, res, next) {
-  const emoji = req.params.emoji;
+router.get("/:emotion", function (req, res, next) {
+  const emotionParam = req.params.emotion;
   // Get the index.html file from the views folder
-  res.render("chat.html", { emoji: emoji });
+  res.render("chat.html", { emotion: emotionParam });
 });
 
 module.exports = router;
