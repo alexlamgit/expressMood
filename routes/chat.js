0edf901fbad06e5c054617d93199b041c882aa70
Alex Lamarche <alexandre_perso@outlook.com>
2023-02-11 23:22:28 -0500

    Styling touches

--- routes/chat.js
+++ routes/chat.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/:emotion", function (req, res, next) {
+  // Get the emotion parameter from the URL
   const emotionParam = req.params.emotion;
+
   // Get the index.html file from the views folder
   res.render("chat.html", { emotion: emotionParam });
 });
